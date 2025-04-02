31be046b9fb8276c32ed0c7cdb16b9750afa6f04
Batuhan Türk <batuay34@outlook.com>
2025-04-02 16:31:23 +0300

    .

--- E-Ticaret.WEBUI/Program.cs
+++ E-Ticaret.WEBUI/Program.cs
@@ -1,7 +1,10 @@
+using E_Ticaret.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DatabaseContext>();
 
 var app = builder.Build();
 
@@ -12,7 +15,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
---
+++ E-Ticaret.WEBUI/Utils/FileHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace E_Ticaret.WEBUI.Utils
+{
+    public class FileHelper
+    {
+        public static async Task<string> FileLoaderASynx(IFormFile formFile, string filePath = "/img/")
+        {
+            string fileName = "";
+            if (formFile != null && formFile.Length > 0)
+            {
+                fileName = formFile.FileName.ToLower();
+                string directory = Directory.GetCurrentDirectory() + "/wwwroot" + filePath;
+                using var stream = new FileStream(Path.Combine(directory, fileName), FileMode.Create);
+                await formFile.CopyToAsync(stream);
+            
+            }
+            return fileName;
+        }
+
+        public static void FileRemover(string fileName ,string filePath = "/img/")
+        {
+            string directory = Directory.GetCurrentDirectory() + "/wwwroot" + filePath + fileName;
+            if (File.Exists(directory))
+            {
+                File.Delete(directory);
+            }
+        }
+    }
+}
