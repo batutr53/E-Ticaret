c856b0097b6e666c7f4b532c9f57ba41f41ad906
Batuhan Türk <batuay34@outlook.com>
2025-04-25 13:52:41 +0300

    .

--- E-Ticaret.Data/Configurations/BrandConfiguration.cs
+++ E-Ticaret.Data/Configurations/BrandConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using E_Ticaret.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace E_Ticaret.Data.Configurations
 {
@@ -13,9 +8,18 @@ namespace E_Ticaret.Data.Configurations
     {
         public void Configure(EntityTypeBuilder<Brand> builder)
         {
-            builder.Property(x=>x.Name).IsRequired().HasMaxLength(100).HasColumnType("character varying(100)");
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100).HasColumnType("character varying(100)");
             builder.Property(x => x.Description).HasMaxLength(200).HasColumnType("character varying(200)");
             builder.Property(x => x.Logo).HasMaxLength(250).HasColumnType("character varying(250)");
+            builder.HasData(new Brand
+            {
+                Id = 1,
+                Name = "Detay Çiçek",
+                Description = "Detay Çiçek",
+                Logo = "",
+                IsActive = true,
+                CreatedDate = new DateTime(2024, 04, 01, 10, 0, 0, DateTimeKind.Utc)
+            });
 
         }
     }
--- E-Ticaret.Data/Configurations/CategoryConfiguration.cs
+++ E-Ticaret.Data/Configurations/CategoryConfiguration.cs
@@ -10,6 +10,17 @@ namespace E_Ticaret.Data.Configurations
         {
             builder.Property(x => x.Name).IsRequired().HasMaxLength(100).HasColumnType("character varying(100)");
             builder.Property(x => x.Image).IsRequired().HasMaxLength(250).HasColumnType("character varying(250)");
+            builder.HasData(new Category {
+                Id = 1,
+                ClassImage = "fa fa-home",
+                Name = "Kategoriler", 
+                Description = "Kategoriler", 
+                IsActive = true, 
+                IsTopMenu = true, 
+                ParentId = 0,
+                OrderNo = 1, 
+                CreatedDate = new DateTime(2024, 04, 01, 10, 0, 0, DateTimeKind.Utc)
+            });
         }
     }
 }
