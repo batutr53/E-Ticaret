a57634661ce6724e24d893340eb9a21523a2fbc5
Batuhan Türk <batuhan.turk@fintechsoft.com>
2025-07-03 10:08:26 +0300

    .

---
+++ E-Ticaret.WEBUI/ViewComponents/CategoriesThree.cs
@@ -0,0 +1,24 @@
+﻿using E_Ticaret.Core.Entities;
+using E_Ticaret.Service.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Ticaret.WEBUI.ViewComponents
+{
+    public class CategoriesThree : ViewComponent
+    {
+        private readonly IService<Category> _categoryService;
+
+        public CategoriesThree(IService<Category> categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _categoryService.GetAllAsync(x => x.IsActive && x.IsTopMenu && x.ParentId == 4);
+            return View(categories);
+        }
+    }
+
+
+}
